dc195a3c463b1c1c9274493b9bb51dba087e50b7
Maricella Escobedo <em126545@student.wpsok.org>
2025-08-13 13:00:58 -0500

    Added HW2

---
+++ HW1.py
@@ -0,0 +1,5 @@
+#Name: Maricella Escobedo
+#Class: 5th Hour
+#Assignment: HW1
+
+print("Hello World!")
---
+++ HW2.py
@@ -0,0 +1,38 @@
+#Name:Maricella Escobedo
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("Hello World!")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+Diana = 67
+Mom = "Hi"
+Cats_are_the_best = True
+
+#3. Print all three variables on the same print function (at the same time).
+print(Diana, Mom, Cats_are_the_best)
+
+#4. Create a variable that asks the user to input an integer.
+Mardi = (int(input("Insert Integer here")))
+print(Mardi)
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+wash = (Mardi + Diana)
+print(wash)
+
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(wash / 2)
+
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+Cats_are_the_best = False
+
+#8. Print the value of the boolean variable.
+print(Cats_are_the_best)
+
+#9. Create a variable with a number that contains decimals.
+Wall = 5.2
+
+#10. Round the number from #9 up or down using the round function.
+print(round(Wall))

6cd858115fd3442473bc1f69be4e58b7050571e8
Maricella Escobedo <em126545@student.wpsok.org>
2025-09-17 12:25:45 -0500

    HW5.py

---
+++ Playground.py
@@ -0,0 +1,25 @@
+#Name: Maricella Escobedo
+#Class: 5th Hour
+#Assignment: Playground
+
+print("No using ChatGPT. " + "No using AI.")
+
+import random
+
+D12 = random.randint(1,6)
+D14 = random.randint(1,6)
+D15 = random.randint(1,6)
+D16 = random.randint(1,6)
+D17 = random.randint(1,6)
+
+randomList = [D12, D14, D15, D16, D17]
+print(randomList)
+
+randomList.sort()
+print(randomList)
+
+randomList[0]=random.randint(1,6)
+print(randomList)
+
+randomList.sort()
+print(randomList)
